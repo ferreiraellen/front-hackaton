21e39279d8bc325a7433802208734f6a551e965d
unknown <ferreira.ellenk@gmail.com>
2022-10-20 14:10:35 -0300

    modificando as rotas

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -16,19 +16,19 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http:netlify.app/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://reciclohack.netlify.app/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http://netlify.app/usuarios/cadastrar', user)
+    return this.http.post<User>('https://reciclohack.netlify.app/usuarios/cadastrar', user)
   }
 
   atualizar(user: User): Observable<User>{
-    return this.http.put<User>('http://netlify.app/usuarios/atualizar', user)
+    return this.http.put<User>('https://reciclohack.netlify.app/usuarios/atualizar', user)
   }
 
   getByIdUser(id: number): Observable<User>{
-    return this.http.get<User>(`http:netlify.app/usuarios/${id}`)
+    return this.http.get<User>(`https://reciclohack.netlify.app/usuarios/${id}`)
   }
 
   logado(){
